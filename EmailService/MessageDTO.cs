bf6e0d0857c7b7d6739cc1d8be54c9498d7c33aa
Yedil Dinmukhamed <54952217+dxrk23@users.noreply.github.com>
2021-01-09 01:50:29 +0600

    Init

---
+++ EmailService/MessageDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmailService
+{
+    public class MessageDTO
+    {
+        public Guid Id { get; set; }
+        public String Type { get; set; }
+        public string JsonContent { get; set; }
+        public DateTime AddedAt { get; set; }
+    }
+}
